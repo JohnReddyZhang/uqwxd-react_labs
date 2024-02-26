b518a81e02248a3f5f1f09f542d16be609e7cb1a
johnreddyzhang <zhzhang135@me.com>
2024-02-26 00:09:55 -0500

    react redux

--- react-redux-master/src/components/MainPanel.js
+++ react-redux-master/src/components/MainPanel.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import MyButton from './MyButton'
+import DivPanel from './DivPanel';
 
+const mainPanel = () => {
+    return (
+        <div>
+            Main Panel <MyButton></MyButton>
+            <DivPanel></DivPanel>
+        </div>
+    )
+}
+
+export default mainPanel;
--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-
-import {createStore} from 'redux';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import myReducers from './reducers'
-
+// import {createStore} from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 
 //Create the store
 const myStore = createStore(myReducers);
 
 //This will console log the current state everytime the state changes
-myStore.subscribe(()=>console.log(myStore.getState()));
+myStore.subscribe(() => console.log(myStore.getState()));
 
 //Enveloping the App inside the Provider, ensures that the states in the store are available
 //throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container)
+root.render(<Provider store={myStore}>
+    <App />
+</Provider>)
+
+// ReactDOM.render(
+//     <Provider store={myStore}>
+//         <App />
+//     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
